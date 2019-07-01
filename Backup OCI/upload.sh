4bc8679a35d913200d02fa015b4b19d14ddb274b
Oleksii Vykhristiyk <avhrst@gmail.com>
2019-07-01 15:29:37 +0300

    Update upload.sh

--- Backup OCI/upload.sh
+++ Backup OCI/upload.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+PATH=$PATH:$HOME/bin
+
+export PATH
+export LANG="en_US"
+export LC_ALL=$LANG.UTF-8
+
 source /home/opc/.bashrc
 /usr/local/bin/oci os object bulk-upload -ns fr03l5aat3bq -bn backup --src-dir /u01/dmp/ --overwrite
+
